170ae7b16b8e6da9aa56e5f2bb01c182880a8a2d
6HellBoy9 <j413507@gmail.com>
2023-10-04 17:59:25 +0300

    task2fix

--- modules/task_2/ezhev_m_shellsimplemerge/shellsimplemerge.h
+++ modules/task_2/ezhev_m_shellsimplemerge/shellsimplemerge.h
@@ -1,6 +1,6 @@
 // Copyright 2023 Ezhev Mikhail
-#ifndef MODULES_TASK_2_EZHEV_M_SHELLSORTSIMPLEMERGE_SHELLSORTSIMPLEMERGE_H_
-#define MODULES_TASK_2_EZHEV_M_SHELLSORTSIMPLEMERGE_SHELLSORTSIMPLEMERGE_H_
+#ifndef MODULES_TASK_2_EZHEV_M_SHELLSIMPLEMERGE_SHELLSIMPLEMERGE_H_
+#define MODULES_TASK_2_EZHEV_M_SHELLSIMPLEMERGE_SHELLSIMPLEMERGE_H_
 
 #include <gtest/gtest.h>
 #include <omp.h>
@@ -13,4 +13,4 @@ std::vector<int> genVector(int size);
 std::vector<int> shellSimpleMergeSeq(const std::vector<int>& vec);
 std::vector<int> shellSimpleMergePar(const std::vector<int>& vec);
 
-#endif  // MODULES_TASK_2_EZHEV_M_SHELLSORTSIMPLEMERGE_SHELLSORTSIMPLEMERGE_H_
+#endif  // MODULES_TASK_2_EZHEV_M_SHELLSIMPLEMERGE_SHELLSIMPLEMERGE_H_
